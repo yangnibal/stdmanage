3335685a4622ec44a61e9f0a3effdc39e797a748
yangnibal <wonjun9670@naver.com>
2020-02-19 13:40:46 +0900

    front edit

--- octo-front/src/pages/AddBooks.js
+++ octo-front/src/pages/AddBooks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import { observable, action } from 'mobx'
+import axios from 'axios'
 
 @observer
 class AddBooks extends React.Component{
@@ -8,9 +9,22 @@ class AddBooks extends React.Component{
     @action handleChange = (e) => {
         const { name, value } = e.target
         this[name] = value
+        this.searchBook(value)
     }
-    @action searchBook = () => {
-
+    @action searchBook = (keyword) => {
+        const _apiKey = '830abcdf4f4174ae428b50d1997d5167';
+        axios.get(`https://dapi.kakao.com/v3/search/book?query=${keyword}`, {
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                Authorization: `KakaoAK ${_apiKey}`
+            }
+        })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
     render(){
         return(
@@ -18,6 +32,7 @@ class AddBooks extends React.Component{
                 <div>
                     <input value={this.keyword} onChange={this.handleChange} name="keyword" placeholder="찾으시는 책 이름을 입력해 주세요..."/>
                 </div>
+                <div></div>
             </div>
         )
     }
